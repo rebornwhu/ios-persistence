b1a2e6cbc632f6549bce03a9c9a0846bcce7fa26
rebornwhu <rebornwhu@gmail.com>
2015-11-14 20:19:57 -0800

    First commit

---
+++ ios-persistence/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ios-persistence
+//
+//  Created by Xiao Lu on 11/14/15.
+//  Copyright © 2015 Xiao Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var lineFields:[UITextField]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
