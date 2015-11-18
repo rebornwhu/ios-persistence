f3ca98bd566b1328ab877cb5ab5494ee72210967
rebornwhu <rebornwhu@gmail.com>
2015-11-17 21:07:50 -0800

    Using archiving instead of plist

--- ios-persistence/ViewController.swift
+++ ios-persistence/ViewController.swift
@@ -11,15 +11,22 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var lineFields:[UITextField]!
+    private let rootKey = "rootKey"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let filePath = self.dataFilePath()
         if NSFileManager.defaultManager().fileExistsAtPath(filePath) {
-            let array = NSArray(contentsOfFile: filePath) as! [String]
-            for var i = 0; i < array.count; i++ {
-                lineFields[i].text = array[i]
+            let data = NSMutableData(contentsOfFile: filePath)!
+            let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
+            let fourLines = unarchiver.decodeObjectForKey(rootKey) as! FourLines
+            unarchiver.finishDecoding()
+            
+            if let newLines = fourLines.lines {
+                for var i = 0; i < newLines.count; i++ {
+                    lineFields[i].text = newLines[i]
+                }
             }
         }
         
@@ -29,8 +36,15 @@ class ViewController: UIViewController {
     
     func applicationWillResignActive(notification:NSNotification) {
         let filePath = self.dataFilePath()
-        let array = (self.lineFields as NSArray).valueForKey("text") as! NSArray
-        array.writeToFile(filePath, atomically: true)
+        let fourLines = FourLines()
+        let array = (self.lineFields as NSArray).valueForKey("text") as! [String]
+        fourLines.lines = array
+        
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+        archiver.encodeObject(fourLines, forKey: rootKey)
+        archiver.finishEncoding()
+        data.writeToFile(filePath, atomically: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,7 +55,7 @@ class ViewController: UIViewController {
     func dataFilePath() -> String {
         let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
         let documentsDirectory = paths[0] as NSString
-        return documentsDirectory.stringByAppendingPathComponent("data.plist") as String
+        return documentsDirectory.stringByAppendingPathComponent("data.archive") as String
     }
 
 }
